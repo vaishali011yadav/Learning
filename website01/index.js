210931bb9bbc6f016c901afcb7f428ea7bd7336b
vaishali011yadav <vaishali011@gmail.com>
2022-02-10 10:03:01 +0530

    learning

---
+++ website01/index.js
@@ -0,0 +1,86 @@
+// starting with js
+// var let const
+// variable declaration
+// 
+var a;   
+// variable initialization
+// a = 10;
+// console.log(typeof a);
+// console.log(a);
+// a="hello";
+// console.log(a);
+// re initialize of variable
+// a="hello"
+// console.log(a);
+
+// typeof operator: it tells us the type of data stored in a variable
+// console.log(typeof a);
+// a=true
+// console.log(a);
+// console.log(typeof a);
+
+// a=null;
+// console.log(a)
+// console.log(typeof a);
+// var num=10;
+// console.log(num);
+// var float=2.4;
+// console.log(float);
+
+// boolean
+// var t=true;
+// var f=false;
+// console.log(t);
+// console.log(f);
+
+// strings
+//  ' '," ",``
+//  single quotes,double quotes,backtick
+// var str='a'
+// console.log(typeof str);
+// str="hhjyvy ?\nhow you doin";
+// back slash n
+// var b=` hellow where are you
+// nbvyyjjgcggcg`;
+// console.log(b);
+// console.log(str);
+var num=678;
+// takes value dynamically
+// console.log(`half of ${num} is ${num/2}`);
+
+// var has some problems;
+
+// 1) redeclaration is allowed;
+var r="hello";
+console.log(r);
+var r=90;
+ console.log(r);
+
+//  overcome karne ke liye
+//  let keyword is used
+let l=900;
+console.log(l);
+//  syntaxError: identifer 'l' has already been declared
+// let l=987;
+// console.log(l);
+
+//  we can reinitialise a variable but we cannot redeclare it
+l=89;
+console.log(l);
+
+// loops
+var num=10;
+// for( var i=0;i<=num;i++){
+//     if(i%2==0){
+//         console.log("num is even");
+//     }
+// }
+
+//  const keyword
+// const a=8;
+// console.log(a);
+// na hi redeclare na hi reintalise
+// typeerror:assignment to const variable
+// a=7;
+
+
