9c4ee0fc76c6fd7e9501f4c854f7aff0cf3e1e9c
vaishali011yadav <vaishali011@gmail.com>
2022-02-12 23:52:28 +0530

    java script and arrays in java done

--- website01/index.js
+++ website01/index.js
@@ -1,86 +1,128 @@
-// starting with js
-// var let const
-// variable declaration
-// 
-var a;   
-// variable initialization
-// a = 10;
-// console.log(typeof a);
-// console.log(a);
-// a="hello";
-// console.log(a);
-// re initialize of variable
-// a="hello"
-// console.log(a);
+{
+    // // staritng JS-> JavaScript
+    // // var let const
 
-// typeof operator: it tells us the type of data stored in a variable
-// console.log(typeof a);
-// a=true
-// console.log(a);
-// console.log(typeof a);
+    // // varibale declaration
+    // var a; //int string float ? 
 
-// a=null;
-// console.log(a)
-// console.log(typeof a);
-// var num=10;
-// console.log(num);
-// var float=2.4;
-// console.log(float);
+    // // variable initalization
+    // a = 10;
+    // // variable reinitalization
+    // a = 20;
+    // console.log(typeof a);
+    // console.log(a);
 
-// boolean
-// var t=true;
-// var f=false;
-// console.log(t);
-// console.log(f);
+    // a = "hello"; //re initialize of varibale 'a'
+    // console.log(a);
 
-// strings
-//  ' '," ",``
-//  single quotes,double quotes,backtick
-// var str='a'
-// console.log(typeof str);
-// str="hhjyvy ?\nhow you doin";
-// back slash n
-// var b=` hellow where are you
-// nbvyyjjgcggcg`;
-// console.log(b);
-// console.log(str);
-var num=678;
-// takes value dynamically
-// console.log(`half of ${num} is ${num/2}`);
+    // // typeof operator-> it tells us the type of data stored in a variable
+    // console.log(typeof a);
 
-// var has some problems;
+    // a = true;
+    // console.log(a);
+    // console.log(typeof a);
 
-// 1) redeclaration is allowed;
-var r="hello";
-console.log(r);
-var r=90;
- console.log(r);
+    // a = null;
+    // console.log(a);
+    // console.log(typeof a);
 
-//  overcome karne ke liye
-//  let keyword is used
-let l=900;
-console.log(l);
-//  syntaxError: identifer 'l' has already been declared
-// let l=987;
-// console.log(l);
+    //Number
+    var num = 10;
+    // console.log(num);
+    var float = 2.4;
+    // console.log(float);
 
-//  we can reinitialise a variable but we cannot redeclare it
-l=89;
-console.log(l);
+    //Boolean
+    var t = true;
+    var f = false;
+    // console.log(t);
+    // console.log(f);
 
-// loops
-var num=10;
-// for( var i=0;i<=num;i++){
-//     if(i%2==0){
-//         console.log("num is even");
-//     }
-// }
+    //String
+    // ' ' , " " , ``
+    // single quotes, double quotes, backtick 
+    var str = 'a';
+    // console.log(typeof str);
+    str = "How you doin ?\ni am fine. how about you ?";
+    //backslash n("\n")
+    // console.log(str);
 
-//  const keyword
-// const a=8;
-// console.log(a);
-// na hi redeclare na hi reintalise
-// typeerror:assignment to const variable
-// a=7;
+    var b = `hi hope you guys are 
+able grasp the concepts`;
+    // console.log(b);
+
+    var num = 4500;
+    // ${}
+    // console.log(`half of ${num} is ${num / 2}`);
+
+
+
+    //var has some problem
+
+    // 1) redeclaration is allowed 
+    var r = "hello";
+    // console.log(r);
+
+    var r = 100;
+    // console.log(r);
+
+    // overcome krne ke liye 
+
+    // let keyword is used
+    let l = 100;
+    // console.log(l);
+
+    // let l = "200"; //SyntaxError: Identifier 'l' has already been declared
+    // console.log(l);
+
+    //we can reinitialize a variable but we cannot redeclare it
+    l = 300;
+    // console.log(l);
+
+    //loops in JS
+    var num = 10;
+
+    // for (var i = 0; i < num; i++){
+    //     if (i % 2 == 0) {
+    //         console.log("num is even");
+    //     }
+    // }
+
+
+    // 2nd problem with var keyword
+    // var keyword is function scoped in case of function else it is global scope 
+    // let keyword is block scoped
+
+    let j = 20;
+    for (var i = 0; i < num; i++) {
+        let j = 20;
+        if (i % 2 == 0) {
+            let hello = 1000;
+            console.log(i);
+            console.log(hello); 
+        }
+        
+        console.log("inner"+j);
+    }
+    console.log("outer "+j);
+    console.log(`value of i is " ${i}`);
+    console.log(hello); // error dega hello is not defined
+
+    // { 
+
+    //     //this space between opening and closing of curly braces is a block
+    // }
+
+    //const keyword
+    const a = 2;
+    // console.log(a);
+    // na hi redeclare na reinitialize
+    // const a = 5;
+    // a = 4; //TypeError: Assignment to constant variable.
+    // console.log("hey");
+
+
+}
+
 
 
